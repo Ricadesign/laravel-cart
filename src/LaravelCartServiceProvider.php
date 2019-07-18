73699bca1b87ee9cbe082ecb82cdb3d74a2bbe8d
Cristian Payet <payet91@gmail.com>
2019-07-18 11:50:17 +0200

    Adding service provider

---
+++ src/LaravelCartServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ricadesign\LaravelCart;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCartServiceProvider extends ServiceProvider 
+{
+     /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        // $this->loadViewsFrom(__DIR__.'/views', 'contact');
+        // $this->publishes([
+        //     __DIR__.'/views' => resource_path('views/vendor/contact'),
+        // ]);
+        // $this->publishes([
+        //     __DIR__.'/config/contact.php' => config_path('contact.php'),
+        // ]);
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->app->make('Ricadesign\Contact\ContactController');
+        // $this->mergeConfigFrom(
+        //      __DIR__.'/config/contact.php', 'contact'
+        //  );
+    }
+}
+
