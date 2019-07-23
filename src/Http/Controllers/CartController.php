16ee2fdcefed16b2b287c39605378ab7b05aeb27
Cristian Payet <payet91@gmail.com>
2019-07-23 16:21:34 +0200

    fix: adding Cart package

--- src/Http/Controllers/CartController.php
+++ src/Http/Controllers/CartController.php
@@ -4,16 +4,30 @@ namespace Ricadesign\LaravelCart;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 
 class CartController extends Controller
 {
     public function index()
     {
+        dd(Cart::getContent());
         return 'Hellow';
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return 'Hellow';
+        // TODO: Add validation
+        $request->validate([
+            'id' => 'required'
+        ]);
+
+        Cart::add([
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'attributes' => array()
+        ]);
+        return 'Store';
     }
 }
