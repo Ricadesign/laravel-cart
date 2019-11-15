21583abf83a67f4fe79c8342de44729bd46b5e4c
Cristian Payet <payet91@gmail.com>
2019-11-15 14:53:08 +0100

    changes

--- vuex-cart/cart.js
+++ vuex-cart/cart.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const uri = '/api/cart';
 export const addItem = item => axios.post(uri, item);
 export const getItems = () => axios.get(uri);
-export const removeItem = id => axios.delete(uri, {id});
+export const removeItem = id => axios({method: 'DELETE', url: uri, data: {id}});
 export const updateItemQuantity = (id, quantity) => axios.put(uri, {id,quantity});
 export const increaseItem = id => axios.put(`${uri}/increment`, {id, quantity:1});
 export const decreaseItem = id => axios.put(`${uri}/increment`, {id, quantity:-1});
